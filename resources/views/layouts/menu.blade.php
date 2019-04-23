f6b775da0421351ec433d09dd931513af73d330b
yd4u2c <yd4u2c@yahoo.comm>
2019-04-23 14:38:01 +0100

    update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,19 +1,29 @@
+@php
+	$user = \Auth::User()->active;
+	//echo $user;
 
-@if(\Auth::User()->active == 0)
+
+if($user == 0)
+{
+	@endphp
 <li><a href="{!! url('/home'); !!}">Dashboard</a></li>
 <li><a href="{!! url('/addaccount'); !!}">Add Account</a></li>
 <li><a href="{!! url('/allaccount'); !!}">View Customer(s)</a></li>
 <li><a href="{!! url('/deposit'); !!}">Deposit</a></li>
 <li><a href="{!! url('/withdraw'); !!}">withdraw</a></li>
-
-@elseif(\Auth::User()->active == 2)
-
+@php
+}
+elseif($user == 2){
+@endphp
 <li><a href="{!! url('/transfer'); !!}">transfer</a></li>
-
-@elseif(\Auth::User()->active == 1)
-
+@php
+}
+elseif($user == 1){
+@endphp
 	<meta http-equiv='refesh'  content="o;url='confirm'">
 
-@endif
+@php
+}
+@endphp
 
 
